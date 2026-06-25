c65608b2758538d57403ebedc9b37cce1c48a5e6
SnivyFilms <Snivy.Films@gmail.com>
2026-06-25 13:02:11 -0400

    More map support

--- README.md
+++ README.md
@@ -12,7 +12,14 @@ TF2 fonts & icons are copyrighted Valve Corporation.
 While I couldn't find any information on who owns mvm.tf, credits to them for the original website.
 
 Non-Valve mvm map credits:
+- mvm_facade made by MegapiemanPHD, [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=3555602427), [TF2Maps](https://tf2maps.net/downloads/facade.19368/).
 - mvm_isolation made by E-Arkham, [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=512476725), [TF2Maps](https://tf2maps.net/downloads/isolation.701/).
+- mvm_powerplant made by Gadget, [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=580052046), [TF2Maps](https://tf2maps.net/downloads/powerplant.147/).
+- mvm_rancher made by Asd417, [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=3549660571), [TF2Maps](https://tf2maps.net/downloads/rancher.15592/).
+- mvm_sewer_invasion and Halloween Version made by s0da72, [TF2Maps](https://tf2maps.net/downloads/sewer-invasion.894/) (Both are in the same download).
 - mvm_skullcove made by Alias, [Steam Workshop](https://steamcommunity.com//sharedfiles/filedetails/?id=455168490), [TF2Maps](https://tf2maps.net/downloads/skullcove.206/).
-- mvm_trainyard made 42, [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=1160890619), [TF2Maps](https://tf2maps.net/downloads/trainyard.1572/).
+- mvm_swirl made by Asd417, [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=3549659438), [TF2Maps](https://tf2maps.net/downloads/swirl.8913/), Halloween Versions: [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=3549659899), [TF2Maps](https://tf2maps.net/downloads/swirl-event.13168/).
+- mvm_trainyard made by 42, [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=1160890619), [TF2Maps](https://tf2maps.net/downloads/trainyard.1572/).
 - mvm_teien made by Freyja, [TF2Maps](https://tf2maps.net/downloads/teien.4023/).
+
+Please note that these maps may have extra relays that cannot be set here, please consider checking the original map page(s) for more information on the map and its relays.
--- js/mvm/datas/maps.js
+++ js/mvm/datas/maps.js
@@ -10,6 +10,13 @@ var MAP_MANNHATTAN = new Map("mvm_mannhattan");
 var MAP_ROTTENBURG = new Map("mvm_rottenburg");
 var MAP_TRAINYARD = new Map("mvm_trainyard_rc9");
 var MAP_TEIEN = new Map("mvm_teien_rc6");
+var MAP_SWIRL = new Map("mvm_swirl_rc7");
+var MAP_SWIRL_EVENT = new Map("mvm_swirl_event_rc8a");
+var MAP_SEWER_INVASION = new Map("mvm_sewer_invasion_b7");
+var MAP_SEWER_INVASION_EVENT = new Map("mvm_sewer_invasion_event_b7");
+var MAP_POWERPLANT = new Map("mvm_powerplant_rc4");
+var MAP_RANCHER = new Map("mvm_rancher_b17");
+var MAP_FACADE = new Map("mvm_facade_b3");
 
 MAP_NO_MAP_SET.addSpawns("spawnbot spawnbot_mission_sniper spawnbot_mission_spy")
 MAP_DECOY.addSpawns("spawnbot spawnbot_mission_sniper spawnbot_mission_spy spawnbot_invasion spawnbot_right spawnbot_left spawnbot_single_flag");
@@ -23,6 +30,13 @@ MAP_MANNHATTAN.addSpawns("spawnbot_main0 spawnbot_main0_squad spawnbot_main1 spa
 MAP_ROTTENBURG.addSpawns("spawnbot flankers spawnbot_chief spawnbot_mission_sentry_buster spawnbot_mission_sniper spawnbot_mission_spy");
 MAP_TRAINYARD.addSpawns("spawnbot gatebot spawnbot_left gatebot_left spawnbot_middle gatebot_middle spawnbot_boss gatebot_boss spawnbot_right gatebot_right spawnbot_mission_spy spawnbot_mission_sniper spawnbot_mission_sentrybuster");
 MAP_TEIEN.addSpawns("spawnbot spawnbot_left spawnbot_lower spawnbot_mission_sniper spawnbot_mission_spy");
+MAP_SWIRL.addSpawns("spawnbot_upper spawnbot_right spawnbot_left spawnbot_main spawnbot_chief spawnbot_sentrybuster spawnbot_flank spawnbot_dud_kill spawnbot_dud spawnbot_ambush_upper spawnbot_ambush_lower");
+MAP_SWIRL_EVENT.addSpawns("spawnbot_upper spawnbot_right spawnbot_left spawnbot_main spawnbot_chief spawnbot_sentrybuster spawnbot_flank spawnbot_dud_kill spawnbot_dud spawnbot_ambush_upper spawnbot_ambush_lower");
+MAP_SEWER_INVASION.addSpawns("spawnbot spawnbot_left spawnbot_right spawnbot_giant_lower spawnbot_mission_spy spawnbot_mission_sniper spawnbot_mission_sniper_upper spawnbot_mission_sniper_lower spawnbot_mission_engineer_upper spawnbot_mission_engineer_lower spawnbot_mission_engineer_lower_left");
+MAP_SEWER_INVASION_EVENT.addSpawns("spawnbot spawnbot_left spawnbot_right spawnbot_giant_lower spawnbot_mission_spy spawnbot_mission_sniper spawnbot_mission_sniper_upper spawnbot_mission_sniper_lower spawnbot_mission_engineer_upper spawnbot_mission_engineer_lower spawnbot_mission_engineer_lower_left");
+MAP_POWERPLANT.addSpawns("spawnbot spawnbot_right spawmbot_single_flag spawnbot_giant spawnbot_main0 spawnbot_main0_squad spawnbot_main1 spawnbot_main2 spawnbot_middle spawnbot_giant spawnbot_invasion spawnbot_main1_slow spawnbot_left spawnbot_upper0 spawnbot_upper1 spawnbot_upper2 flankers spawnbot_mission_spy spawnbot_mission_sentrybuster spawnbot_mission_sentry_buster spawnbot_mission_sniper spawnbot_mission_sniper0 spawnbot_mission_sniper1");
+MAP_RANCHER.addSpawns("spawnbot_main spawnbot_left spawnbot_center spawnbot_right spawnbot_chief");
+MAP_FACADE.addSpawns("spawnbot spawnbot_giant spawnbot_left spawnbot_right spawnbot_mission_sentrybuster spawnbot_mission_sniper spawnbot_mission_spy");
 
 MAP_DECOY.addTags("nav_prefer_flank_right nav_prefer_flank_left");
 MAP_COALTOWN.addTags("nav_prefer_flank_right nav_prefer_flank_left special_main_right special_main_left");
@@ -34,6 +48,10 @@ MAP_MANNHATTAN.addTags("nav_prefer_gate1_flank");
 MAP_ROTTENBURG.addTags("Flankers");
 MAP_TRAINYARD.addTags("bot_flank bot_right bot_left");
 MAP_TEIEN.addTags("nav_prefer_flank_left nav_prefer_flank_right");
+MAP_SWIRL.addTags("nav_prefer_gate1_flank");
+MAP_SWIRL_EVENT.addTags("nav_prefer_gate1_flank");
+MAP_POWERPLANT.addTags("common bomb_carrier nav_prefer_right nav_prefer_flank_right nav_prefer_left nav_prefer_flank_left nav_prefer_final_flank_right nav_prefer_final_alt nav_prefer_final_main");
+MAP_FACADE.addTags("flankers bot_teletank");
 
 MAP_NO_MAP_SET.addTankPaths("boss_path_1");
 MAP_DECOY.addTankPaths("boss_path_1");
@@ -46,7 +64,13 @@ MAP_SKULLCOVE.addTankPaths("tank_path_left tank_path_right");
 MAP_ROTTENBURG.addTankPaths("tank_path_a_10 tank_path_b_10");
 MAP_TRAINYARD.addTankPaths("tank_path_LL_1 tank_path_LU_1 tank_path_RL_1 tank_path_RU_1");
 MAP_TEIEN.addTankPaths("boss_path_a1 tank_path_b1");
-
+MAP_SWIRL.addTankPaths("path_tank_upper path_tank_lower path_tank_default path_tank_reverse");
+MAP_SWIRL_EVENT.addTankPaths("path_tank_upper path_tank_lower path_tank_default path_tank_reverse");
+MAP_SEWER_INVASION.addTankPaths("tank_path_a_1");
+MAP_SEWER_INVASION_EVENT.addTankPaths("tank_path_a_1");
+MAP_POWERPLANT.addTankPaths("boss_path_1");
+MAP_RANCHER.addTankPaths("path_tank_left path_tank_right tank_start_harvester");
+MAP_FACADE.addTankPaths("boss_path_1 boss_path_a_1");
 
 MAP_NO_MAP_SET.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finished_relay");
 MAP_DECOY.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finished_relay");
@@ -60,18 +84,35 @@ MAP_MANNHATTAN.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finish
 MAP_ROTTENBURG.setStartWaveOutput("wave_start_relay_classic").setDoneOutput("wave_finished_relay");
 MAP_TRAINYARD.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finished_relay");
 MAP_TEIEN.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finished_relay");
+MAP_SWIRL.setStartWaveOutput("wave_start_relay", true).setStartWaveOutput("wave_start_relay_notimer", false);
+MAP_SWIRL_EVENT.setStartWaveOutput("wave_start_relay", true).setStartWaveOutput("wave_start_relay_notimer", false);
+MAP_SEWER_INVASION.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finished_relay");
+MAP_SEWER_INVASION_EVENT.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finished_relay");
+MAP_POWERPLANT.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finished_relay");
+MAP_RANCHER.setStartWaveOutput("wave_start_relay").setStartWaveOutput("wave_start_relay_timer", true).setDoneOutput("wave_finish_relay");
+MAP_FACADE.setStartWaveOutput("wave_start_relay").setDoneOutput("wave_finish_relay");
 
+//This shows on the website. Sorted by alphabetical order.
+//Official Maps
 MapList[""] = MAP_NO_MAP_SET;
-MapList["mvm_decoy"] = MAP_DECOY;
-MapList["mvm_coaltown"] = MAP_COALTOWN;
-MapList["mvm_ghostown"] = MAP_GHOSTOWN;
-MapList["mvm_mannworks"] = MAP_MANNWORKS;
 MapList["mvm_bigrock"] = MAP_BIGROCK;
-MapList["mvm_isolation_b4"] = MAP_ISOLATION;
-MapList["mvm_skullcove"] = MAP_SKULLCOVE;
+MapList["mvm_coaltown"] = MAP_COALTOWN;
+MapList["mvm_decoy"] = MAP_DECOY;
+MapList["mvm_ghostown"] = MAP_GHOSTOWN;
 MapList["mvm_mannhattan"] = MAP_MANNHATTAN;
+MapList["mvm_mannworks"] = MAP_MANNWORKS;
 MapList["mvm_rottenburg"] = MAP_ROTTENBURG;
+//Community Maps
+MapList["mvm_facade_b3"] = MAP_FACADE;
+MapList["mvm_isolation_b4"] = MAP_ISOLATION;
+MapList["mvm_powerplant_rc4"] = MAP_POWERPLANT;
+MapList["mvm_sewer_invasion_b7"] = MAP_SEWER_INVASION;
+MapList["mvm_sewer_invasion_event_b7"] = MAP_SEWER_INVASION_EVENT;
+MapList["mvm_skullcove"] = MAP_SKULLCOVE;
+MapList["mvm_swirl_rc7"] = MAP_SWIRL;
+MapList["mvm_swirl_event_rc8a"] = MAP_SWIRL_EVENT;
 MapList["mvm_trainyard_rc9"] = MAP_TRAINYARD;
+MapList["mvm_teien_rc6"] = MAP_TEIEN;
 
 MAP_DECOY.addPopulation("Doe's Drill", "mvm_decoy.xml");
 MAP_DECOY.addPopulation("Disk Deletion", "mvm_decoy_advanced.xml");
